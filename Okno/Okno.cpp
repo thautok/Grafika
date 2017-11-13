af5849596685e159c8f3406247b4109d391cfea1
Natalia Krupa <nala5@tlen.pl>
2017-11-13 14:50:45 +0100

    First steps

--- Okno/Okno.cpp
+++ Okno/Okno.cpp
@@ -2,10 +2,50 @@
 //
 
 #include "stdafx.h"
+#include "Okno.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char** argv)
 {
+	Okno okno(768, 1024, 100, 100);
+	okno.stworzenieOkna(argc, argv);
+	glutDisplayFunc(okno.wyswietl);
+
+	glClearColor(1.0f, 0.8f, 0.9f, 0.0f); 
+
+	glutMainLoop();
+
 	return 0;
 }
 
+Okno::Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY)
+{
+	this->wysokoscOkna = wysokoscOkna;
+	this->szerokoscOkna = szerokoscOkna;
+	this->polozenieOknaX = polozenieOknaX;
+	this->polozenieOknaY = polozenieOknaY;
+}
+
+Okno::Okno()
+{
+	wysokoscOkna = 768;
+	szerokoscOkna = 1024;
+	polozenieOknaX = 100;
+	polozenieOknaY = 100;
+}
+
+void Okno::stworzenieOkna(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("okno");
+}
+
+void Okno::wyswietl()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+
+}
---
+++ Okno/Okno.h
@@ -0,0 +1,26 @@
+#ifndef OKNO_H
+#define OKNO_H
+
+#include <GL/freeglut.h>
+
+class Okno
+{
+private:
+	static int wysokoscOkna;
+	static int szerokoscOkna;
+	int polozenieOknaX;
+	int polozenieOknaY;
+
+public:
+	Okno(void);
+	Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY);
+
+	~Okno(void);
+	void stworzenieOkna(int argc, char** argv);
+	static void wyswietl();
+
+};
+
+
+
+#endif /* OKNO_H */

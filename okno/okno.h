7589272cc26c4c694f6ee13d9f24b84ecc434757
Natalia <nala5@tlen.com>
2017-11-27 13:58:38 +0100

    test

---
+++ okno/okno.h
@@ -0,0 +1,25 @@
+#ifndef OKNO_H
+#define OKNO_H
+
+#include <GL/freeglut.h>
+
+class Okno
+{
+private:
+	static int wysokoscOkna;
+	static int szerokoscOkna;
+	int polozenieOknaX;
+	int polozenieOknaY;
+
+public:
+	Okno(void);
+	Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY);
+
+	void stworzenieOkna(int argc, char** argv);
+	static void wyswietl();
+
+};
+
+
+
+#endif /* OKNO_H */
